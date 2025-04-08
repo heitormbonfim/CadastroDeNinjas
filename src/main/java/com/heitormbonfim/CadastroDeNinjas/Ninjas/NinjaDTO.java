f645cdd86963a9914fd83cfab1e5922f8cef4af6
Heitor M Bonfim <71136688+heitormbonfim@users.noreply.github.com>
2025-04-08 16:19:00 -0300

    chore: includes the DTO version and mapping manually the paths

--- src/main/java/com/heitormbonfim/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/com/heitormbonfim/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,67 @@
 package com.heitormbonfim.CadastroDeNinjas.Ninjas;
 
+import com.heitormbonfim.CadastroDeNinjas.Missions.MissionModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private MissionModel mission ;
+    private String rank;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public MissionModel getMission() {
+        return mission;
+    }
+
+    public void setMission(MissionModel mission) {
+        this.mission = mission;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
 }
