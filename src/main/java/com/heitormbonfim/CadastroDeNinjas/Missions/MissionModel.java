e0c8819bd268eebed81ce40091cb5111caa6935d
Heitor M Bonfim <71136688+heitormbonfim@users.noreply.github.com>
2025-04-17 20:44:47 -0300

    fix: replaced the NinjaDTO type with NinjaModel, because it was not an entity

--- src/main/java/com/heitormbonfim/CadastroDeNinjas/Missions/MissionModel.java
+++ src/main/java/com/heitormbonfim/CadastroDeNinjas/Missions/MissionModel.java
@@ -1,7 +1,6 @@
 package com.heitormbonfim.CadastroDeNinjas.Missions;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.heitormbonfim.CadastroDeNinjas.Ninjas.NinjaDTO;
 import com.heitormbonfim.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -30,7 +29,7 @@ public class MissionModel {
     // Uma missão pode ter vários ninjas
     @OneToMany(mappedBy = "mission")
     @JsonIgnore // evita loop de serialização
-    private List<NinjaDTO> ninjas;
+    private List<NinjaModel> ninjas;
 
     public void setAddObjective(String objective) {
         this.objectives.add(objective);
