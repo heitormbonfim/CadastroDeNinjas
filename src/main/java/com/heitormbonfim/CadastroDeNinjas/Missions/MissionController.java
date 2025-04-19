a89970b9f59f0b3d1ce5f5e7bba820d9c2ec46a4
Heitor M Bonfim <71136688+heitormbonfim@users.noreply.github.com>
2025-04-18 21:09:39 -0300

    feat: added update and delete methods in mission controller

--- src/main/java/com/heitormbonfim/CadastroDeNinjas/Missions/MissionController.java
+++ src/main/java/com/heitormbonfim/CadastroDeNinjas/Missions/MissionController.java
@@ -1,9 +1,11 @@
 package com.heitormbonfim.CadastroDeNinjas.Missions;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,5 +40,46 @@ public class MissionController {
     public ResponseEntity<?> getAllMissions() {
         List<MissionDTO> missions = missionService.listAllMissions();
         return ResponseEntity.ok(missions);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update mission", description = "Update the mission saved in the database")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "mission updated successfully"),
+            @ApiResponse(responseCode="400", description = "missing attributes")
+    })
+    public ResponseEntity<?> updateMission(
+            @Parameter(description = "User sends the id in the request")
+            @PathVariable Long id,
+            @Parameter(description = "User sends the data in the request body")
+            @RequestBody MissionDTO missionDTO
+    ) {
+        MissionDTO missionExists = missionService.findMissionById(id);
+
+        if (missionExists != null) {
+            MissionDTO mission = missionService.updateMission(id, missionDTO);
+            return ResponseEntity.ok(mission);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Mission not found");
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete mission", description = "removes the mission from the database")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description="Mission delete"),
+            @ApiResponse(responseCode = "400", description = "Mission not found")
+    })
+    public ResponseEntity<?> deleteMission(
+            @Parameter(description="User sends the id of the mission to delete")
+            @PathVariable Long id
+    ) {
+        MissionDTO missionExists = missionService.findMissionById(id);
+        if (missionExists != null) {
+            missionService.deleteMissionById(id);
+            return ResponseEntity.ok("Mission deleted");
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Mission not found");
     }
 }
